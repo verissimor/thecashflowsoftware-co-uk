72205da880b651cc184e7c70389b47be6c37937d
Verissimo Ribeiro <vribeiro@landytech.com>
2023-10-30 20:28:06 +0000

    fix(build): adding https prefix to versel url

--- src/lib/url-shared-utils.js
+++ src/lib/url-shared-utils.js
@@ -1,4 +1,4 @@
 export const siteUrl =
-  process?.env?.VERCEL_URL ||
-  process?.env?.NEXT_PUBLIC_VERCEL_URL ||
+  `https://${process?.env?.VERCEL_URL}` ||
+  `https://${process?.env?.NEXT_PUBLIC_VERCEL_URL}` ||
   "http://localhost:3000";
